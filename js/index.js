0af3237d593925169e5ed79ace345690a31288fc
Anna Nykolayenko <mailtoannan@gmail.com>
2018-10-11 22:33:45 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	$("#typed").typed({
+
+		stringsElement: $("#typed-strings"),
+		typeSpeed: 35,
+		backDelay: 495,
+		loop: false,
+		contentType: "html",
+		loopCount: false
+	});
+});
